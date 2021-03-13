c5d43739294781d6f61ff1b3dba35235851369f3
Nelson Rodrigues <nelson@waveweb2.com>
2021-03-13 21:51:07 +0000

    Show the number of active stylesheets in the extension icon

--- src/background.js
+++ src/background.js
@@ -4,36 +4,44 @@
 let activeTabs = {};
 let browser = chrome || browser;
 
+function getActiveStylesheetsCount(tabId) {
+    if (activeTabs[tabId]) {
+        return activeTabs[tabId].length.toString();
+    }
+
+    return '';
+}
+
 browser.runtime.onMessage.addListener((message, sender) => {
-    if (message.action === 'pageLoad') {
-        if (activeTabs[sender.tab.id] !== undefined) {
-            browser.browserAction.setBadgeText({ text: 'ON', tabId: sender.tab.id });
-            browser.tabs.sendMessage(sender.tab.id, { action: 'inject', urlList: activeTabs[sender.tab.id] });
-        }
+    const tabId = message.tabId || sender.tab.id;
+
+    if (message.action === 'pageLoad' && activeTabs[tabId]) {
+        browser.browserAction.setBadgeText({ text: getActiveStylesheetsCount(tabId), tabId: tabId });
+        browser.tabs.sendMessage(tabId, { action: 'inject', urlList: activeTabs[tabId] });
     }
 
     if (message.action === 'inject') {
-        if (!activeTabs[message.tabId]) {
-            activeTabs[message.tabId] = [];
+        if (!activeTabs[tabId]) {
+            activeTabs[tabId] = [];
         }
 
-        activeTabs[message.tabId].push(message.url);
-        browser.browserAction.setBadgeText({ text: 'ON', tabId: message.tabId });
-        browser.tabs.sendMessage(message.tabId, { action: 'inject', urlList: [message.url] });
+        activeTabs[tabId].push(message.url);
+        browser.browserAction.setBadgeText({ text: getActiveStylesheetsCount(tabId), tabId: tabId });
+        browser.tabs.sendMessage(tabId, { action: 'inject', urlList: [message.url] });
     }
 
-    if (message.action === 'clear') {
-        activeTabs[message.tabId].forEach((stylesheetURL, index) => {
+    if (message.action === 'clear' && activeTabs[tabId]) {
+        activeTabs[tabId].forEach((stylesheetURL, index) => {
             if (stylesheetURL === message.url) {
-                activeTabs[message.tabId].splice(index, 1);
+                activeTabs[tabId].splice(index, 1);
             }
         });
 
-        if (activeTabs[message.tabId].length < 1) {
-            delete activeTabs[message.tabId];
+        if (activeTabs[tabId].length < 1) {
+            delete activeTabs[tabId];
         }
 
-        browser.browserAction.setBadgeText({ text: '', tabId: message.tabId });
-        browser.tabs.sendMessage(message.tabId, { action: 'clear', url: message.url });
+        browser.browserAction.setBadgeText({ text: getActiveStylesheetsCount(tabId), tabId: tabId });
+        browser.tabs.sendMessage(tabId, { action: 'clear', url: message.url });
     }
 });
