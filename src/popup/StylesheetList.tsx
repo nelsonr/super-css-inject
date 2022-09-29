f72716cc75ee82587f938beeff7f127324974694
Nelson Rodrigues <nelsonrodrigues87@outlook.com>
2022-09-29 20:00:59 +0100

    Rename selection handler in Popup/StylesheetList

--- src/popup/StylesheetList.tsx
+++ src/popup/StylesheetList.tsx
@@ -9,10 +9,10 @@ interface IProps {
 }
 
 export function StylesheetList (props: IProps) {
-    const { list, onSelectionChange: onSelectionToggle, activeList } = props;
+    const { list, onSelectionChange, activeList } = props;
 
     const stylesheets = list.map((stylesheet: Stylesheet, index: number) => {
-        const handleActiveChange = (isActive: boolean) => onSelectionToggle(isActive, index);
+        const handleActiveChange = (isActive: boolean) => onSelectionChange(isActive, index);
         const isActive: boolean = activeList.find((url: string) => url === stylesheet.url) !== undefined;
         
         return <StylesheetItem key={index} stylesheet={stylesheet} active={isActive} onActiveToggle={handleActiveChange} />;
