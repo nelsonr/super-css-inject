fb8e1b031bf9feca9491d5146a23dea9605ddcd8
Nelson Rodrigues <nelsonrodrigues87@outlook.com>
2022-10-09 13:59:50 +0100

    Update URL validation; Document utils functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,33 @@
 import { Tab } from "./types";
 
+/**
+ * Alias for accessing the browser extension API.
+ * 
+ * Chrome uses `chrome`. Firefox uses `browser`.
+ */
 export const env = chrome || browser;
 
+/**
+ * Extracts the last portion of the URL, if the last part of the URL
+ * is a filename (eg.: `theme.css`) it will return that as result.
+ * 
+ * Example: "http://localhost/my-theme.css" => "my-theme.css"
+ * 
+ * @param url A stylesheet URL
+ * @returns The last portion of the URL
+ */
 export function getStylesheetName(url: string) {
     const urlParts = url.split("/");
     return urlParts[urlParts.length - 1];
 }
 
+/**
+ * Sorts URLs by the last part, aka the filename.
+ * 
+ * @param urlA URL string
+ * @param urlB URL string
+ * @returns Order between the two strings
+ */
 export function sortByName (urlA: string, urlB: string) {
     const nameA = getStylesheetName(urlA).toLowerCase();
     const nameB = getStylesheetName(urlB).toLowerCase();
@@ -21,7 +42,7 @@ export function sortByName (urlA: string, urlB: string) {
 }
 
 /**
- * Get the injected stylesheets for a tab
+ * Get the injected stylesheets for a browser tab
  * 
  * @returns Object with list of stylesheets active per browser tab
  */
@@ -44,6 +65,12 @@ export function getTabInjectedStylesheets (tabId: number): Promise<string[]> {
     });
 }
 
+/**
+ * Tries to get the current active browser tab (if any).
+ * It returns either a Tab object or a undefined result, wrapped in a Promise.
+ * 
+ * @returns Tab information wrapped in a Promise
+ */
 export async function getCurrentTab (): Promise<Tab> {
     const queryOptions = { active: true, lastFocusedWindow: true };
     
@@ -53,10 +80,25 @@ export async function getCurrentTab (): Promise<Tab> {
     return tab;
 }
 
+/**
+ * Helper function to create a CSS classes string from an array.
+ * 
+ * Example: `["class-a", "class-b"]` => `"class-a class-b"`
+ * 
+ * @param classes An array of CSS classes
+ * @returns A string with CSS classes separated by spaces
+ */
 export function setCSSClasses (classes: string[]): string {
     return classes.join(" ").trim();
 }
 
+/**
+ * Sets the badge text for the extension icon on a specific browser tab.
+ * Used mainly to highlight the current number of injected Stylesheets on a tab.
+ * 
+ * @param tabId Browser tab identifier
+ * @param text The text content for the badge
+ */
 export function updateBadgeText (tabId: number, text: string) {
     env.action.setBadgeText({
         tabId: tabId,
@@ -66,13 +108,13 @@ export function updateBadgeText (tabId: number, text: string) {
 
 /**
  * Validates a CSS file URL.
- * It accepts anything that starts with either "http://" or "https://" and ends with ".css".
+ * It accepts anything that starts with either "http://" or "https://".
  * 
  * @param url The URL string to validate
  * @returns true or false
  */
 export function validateURL(url: string): boolean {
-    const urlRegex = /^https?:\/\/.*\.css/gi;
+    const urlRegex = /^https?:\/\//gi;
 
     return url.match(urlRegex) !== null;
 }
